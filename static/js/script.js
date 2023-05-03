fc001c93d387585c6b8c1ebed742ca1b9d62e2b8
Mi Yu <mi@afteryu.me>
2023-05-03 12:04:22 -0400

    refactor: split ToC-related functions

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.footnote-definition > p').forEach((p) => {
+    let span = document.createElement('a');
+    span.addEventListener("click", () => {
+      document.querySelector('a[href="#' + p.parentElement.id + '"]')?.scrollIntoView({ block: 'center' });
+    })
+    span.classList.add('footnote-back-button');
+    span.textContent = 'Go Back';
+    p.appendChild(span);
+  });
+});
