14b33518a590dd87275c97ff5a168e52b18cbae4
Mi Yu <miyu.chs@gmail.com>
2024-04-15 20:33:41 -0400

    refactor: use CSS directly instead of SASS

--- static/js/toc.js
+++ static/js/toc.js
@@ -1,26 +1,40 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const toc_observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      const id = entry.target.getAttribute('id');
+function setResponsiveToc(toc_element) {
+  if (window.innerWidth < 800) {
+    // mobile windows default to hiding the ToC
+    return toc_element.classList.replace("toc-show", "toc-hide");
+  } else {
+    // reset ToC if windows get bigger
+    return toc_element.classList.replace("toc-hide", "toc-show");
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const toc_observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
       if (entry.intersectionRatio > 0) {
-        document.querySelector('.toc a[href$=' + id + ']').classList.add('active')
+        document
+          .querySelector(".toc a[href$=" + id + "]")
+          .classList.add("active");
       } else {
-        document.querySelector('.toc a[href$=' + id + ']').classList.remove('active')
+        document
+          .querySelector(".toc a[href$=" + id + "]")
+          .classList.remove("active");
       }
     });
   });
 
   // Track all sections that have an `id` applied
-  document.querySelectorAll('.has-toc h2[id],h3[id]').forEach((section) => {
+  document.querySelectorAll(".has-toc h2[id],h3[id]").forEach((section) => {
     toc_observer.observe(section);
   });
 
-  const title_observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      const toc_header = document.querySelector('.toc > h2');
+  const title_observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const toc_header = document.querySelector(".toc > h2");
       if (toc_header !== null) {
         if (entry.intersectionRatio > 0) {
-          toc_header.textContent = 'Table of Contents';
+          toc_header.textContent = "Table of Contents";
         } else {
           toc_header.textContent = entry.target.textContent;
         }
@@ -28,13 +42,21 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  const title = document.querySelector('.has-toc h1');
+  const title = document.querySelector(".has-toc h1");
   if (title !== null) {
     title_observer.observe(title);
   }
 
-  document.querySelector('.toc > h2')?.addEventListener("click", () => {
-    const className = 'user-toggle-toc';
-    document.querySelector('.toc').classList.toggle(className);
+  const toc_element = document.querySelector(".toc");
+
+  document.querySelector(".toc > h2")?.addEventListener("click", () => {
+    if (toc_element.classList.contains("toc-show")) {
+      return toc_element.classList.replace("toc-show", "toc-hide");
+    } else {
+      return toc_element.classList.replace("toc-hide", "toc-show");
+    }
   });
+
+  setResponsiveToc(toc_element);
+  window.addEventListener("resize", () => setResponsiveToc(toc_element));
 });
