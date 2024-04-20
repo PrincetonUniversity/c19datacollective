25d97284d242a25262c0cdb4482df80ab90a9453
Mi Yu <miyu.chs@gmail.com>
2024-04-20 13:39:59 -0400

    format: reformat script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,13 @@
-window.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.footnote-definition > p').forEach((p) => {
-    let span = document.createElement('a');
+window.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".footnote-definition > p").forEach((p) => {
+    let span = document.createElement("a");
     span.addEventListener("click", () => {
-      document.querySelector('a[href="#' + p.parentElement.id + '"]')?.scrollIntoView({ block: 'center' });
-    })
-    span.classList.add('footnote-back-button');
-    span.textContent = 'Go Back';
+      document
+        .querySelector('a[href="#' + p.parentElement.id + '"]')
+        ?.scrollIntoView({ block: "center" });
+    });
+    span.classList.add("footnote-back-button");
+    span.textContent = "Go Back";
     p.appendChild(span);
   });
 });
